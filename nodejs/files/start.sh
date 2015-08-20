021c57527b5b2391617ace40ca26a9e46783b20e
Ricardo Amaro <mail@ricardoamaro.com>
2015-08-20 21:13:16 +0100

    fixed nodejs start

--- nodejs/files/start.sh
+++ nodejs/files/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-export DRUSH="/.composer/vendor/drush/drush/drush"
 export LOCAL_IP=$(hostname -I)
 export IDENTIFIER=${IDENTIFIER:-"build_$(date +%Y_%m_%d_%H%M%S)"}
 export REPO=${REPO:-"git@github.com:acquia/network-nodejs-uptime-scanner.git"}
-export APP=${APP:-"API"}
+export APP=${APP:-"nodejs"}
 export CMD=${CMD:-""}
 export DBUSER=${DBUSER:-"someuser"}
 export DBPASS=${DBPASS:-"somepass"}
@@ -15,15 +14,12 @@ cd /data
 echo "$GITKEY" > /root/gitkey
 GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa_example -F /dev/null" git clone $REPO /data
 
-#if [ ! -f /var/www/html/sites/default/settings.php ]; then
-  #GIT CLONE
-  #MAKE STUFF WORK FOR APPX
-  #sleep 3s
-#else
-#fi
-
 echo
 echo "--------------------------STARTING SERVICES-----------------------------------"
 echo "USE CTRL+C TO STOP THIS APP"
 echo "------------------------------------------------------------------------------"
-supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+## supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+npm install
+node app.js --dispatcher-url http://localhost:8888
+#It will eventually include more arguments
+#(like --dispatcher-username, --dispatcher--password, --processor-url etc...)
