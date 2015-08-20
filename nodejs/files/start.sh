0f0811c04cfc45a2154897f8e8d4ff7c5037e671
Ricardo Amaro <mail@ricardoamaro.com>
2015-08-20 22:10:10 +0100

    fixed nodejs git ssh

--- nodejs/files/start.sh
+++ nodejs/files/start.sh
@@ -1,5 +1,57 @@
 #!/bin/bash
 
+GITKEY="-----BEGIN RSA PRIVATE KEY-----
+MIIJKQIBAAKCAgEAw4sBT6AKxyJJKe6eMssDgMItVYyt8F9t5VimULKTuMk2BCa/
+ICG6dH2M3D6d8cWonj90iCe9c59ZWALcXA4scaey9SbEmjya78XQmftsGlaZSDlX
+zvpjDp6PFUPEBVYjvJ67J6748RejTGiHfYzUYi6krOnhL0IiZ1ibIGKHruuG/J49
+7we4L8nhlxeludEFxfd22OEZA/fcJsrcl8en4QljxQ24ZKRc5BED2WTZhYRO3gHS
+dh7ebU8nqTrXrE8KhEJmxJoLG6kqOpnBhXT8mrnW9YdSZ8yLiBKwDZ3Epsv6dgGv
+dWW+N/cJss0xX86zOoBgIR4IHfJ/7Tq3zQsqf7S7hKQktqVA4lfG+G3mvpC8Alva
+rm8mitQ+hwdZFRwOilH0Lh7W0ZnT8RuSmUHzp+aFZ9sFjZjBLRxUO50AO4MHDraX
+loeYf8cG9I+zOi/rbsW3KK/BpXUXO0rj85RDhE62kuwJxECKRrMV+dJPbdC2F8Cv
+k7CfOpSarEJmGyIwF3Z3Qc+nsQoRbJFDktlDrqKYQjH/oytEG2PhaGthhKTzH/zg
+WBhwES+SiPa3xZtq3weDFfBF8qCJ73NKsPqKj3y8SnTGofj5/0qt/tFG5nB2Z/TB
+aW0cb62x6K6gaYs6Qy7zDpxLKKpK848ZdP3C4Cfbggsrserhvu/kusgEQYUCAwEA
+AQKCAgA+jCEgOh4YclQa0JPTxXJaZkIgAj7FC5FYchHI8EYmLl5T15z9ByzgBCTh
+cjoNUt9LtxpsTuHfsRHgABJCc9othKnOltb+SvJ4Tbsa9fwvNJIVMxpuP1MeIwO7
+lpCF8Ha/57oPyH0iiSAX2bu1Ii3xNeBA2xLHjgz5J7N/GWUzQ21LMsa2xa5Zwuag
+8NR26nBBUZW3a9EXntnRosQK3LdLWTiLmIbp0lVQMk0b5ZFajua3RrfuRpN5PX9/
+Ny4zlJjVk3PR4UA6WdMDKZloOfJu/5DNurpWL2xWWzvrLxLvE4IK6yEXmTLOLfvx
+MIdhkPltdhDJpxSLl7zpwgV21eKCpUrx4vNSq4Z753UaMyl0oWdFOPN2edQdE1dC
+pUhrQdwFj5Up5LeW/L4CsE/gUkGvR+YD09DVYl4xK06mbpukJFatFuh1tlSnMEk/
+AkvRYs/bvSGrYjkGvacig2QMm2pyinxWyqtXwkebxCHSsEE6Z69bsHnYKQ0YzMcf
+4ikpf/TkwP9hzwDXBdWS3CXLLRK17M+NNfmVIfYfEvJViIDLfezsLeV9AJ1pmxAs
+fYc1UpjbKb9oi2dUX/CVXYb1F6fqAqez1g5Jndvo6Mf4EG7QWEtPqNlZAHSqMJ51
+cntIpT6sZ159x/4kniv7wI47mC2cTtO1zvb+uc5eJTOqmpgtwQKCAQEA8vY7798m
+nVABsaujoqmPCWo1iYFkDQEv5snA2qWXzfQAN4/EQmHOzteoch6OG/sA+5MbeV6Z
+MjkSf8mQLaNxg1iuVjw0fC9A61EZP0ldP6V+R2zJffYps6+NUnfs1fk7ezHqhdYQ
+nVSa0mdh6CcU8QB/M3F9PZPwu2+H/slqxmw2Tcayzd8n+NJarmQMOjpMlf4z2pk+
+lP3hdChSUl1rXn+ukShmQlqUfj0WY12Qfykfe0fsCfigz3Xl3eSSX+H7I7d41S2l
+Q01XZBxbmVphc9Sm53j+uY5z1yHq10XsgX07/3txkDnyZOgLDPRcYePWpffkFEnv
+Yw1WClHVdsnJNQKCAQEAzglW2NHVjkVd+BLV+zlgyAH2b/wrPT8M5ku60rKtSFkx
+rk6ofFfBLH+Ot9tGp97Eq9fvnwil753PcMnPEQL/UQooFY1C6zQhFg3Q6kMnp/Nn
+cDFhPDgJSMCFEOZwezWDLQu4FE6DloRUCjty3+/rcXl/AZqllmHo8ptl/26Un6J1
+dnQ4tlqp7w7GcaXVG/27l+kOHXsjyJWqNnAKYE484GcIzYdH91gmT7z8fUCZydX+
+FxlSF3HS7ko0w4tJCIecdjWAhsslUzDEnWVoWguzDJW88lr3+Nyb33HjYOmix2MB
+a7wVktb3Lhc7QBDlSf/hp4B46std6xyxcPa7LsfxEQKCAQA/+76fFgpKRWaczpHC
+ZZropNLCipO5pQVjZuBl9FP9BfQErdTGkpFvAXY3z3onPjvtBLmohdQcWNFZW9Cf
+ZDqhl3oZ9bLwvVUAY5QdiYmIGGIgFzMc+/XRfzrda/2v//m9INEdO9mWRVCcdmNf
+SuOF8FaPSqTr0YkFI4MSAyMebCME6n3uH4WcTnFZGbnqn63Jlm87cz366L015OSB
+El8TPNbRHW2IregLMrUxp9NE2aSTx8YOfi6DVA4cGHS/0Xhc2JV2tpWeGsEX+Sva
+P+YIJglqTf/vB+fT0PfSh3SYSU3Co1St4cTG9CDs4XFuyhyeCqFDApGLaViuUQzB
+QrS9AoIBAQDJmovXRqNfPfLv7YZh90gDDSCVfTrWCB7YLVvWFS3HcDdTpvXoDXog
+z4PX1YDye+EXgQigJbcwmstDQsURkUxAgN6uD/Zd7Tk+ujAlOabRCTO6rSbG5NH4
+1SCfY8zGUptTdXcer6DEpCETjg4en0VTWJ309Nx4JL/qvDH22b4l4bJsQNbJYDeN
+iniU9b5sUD/CKjI8mKRv/jp6X/FXQNcCgO81JN0/YorCVVr+ODsDKxgghA82j5Lk
+xcmRWXYSGZf4duuXeanBGCQbGwIbsfz2IIKs5mRjp4ICBw+FYbd8wBzAzV6SuzNM
+b0DztFSXrwCfhO9MV8hDaWUa8uI/qTbxAoIBAQDpbdpPtZA/QL1pkk/kxJIcvIMr
+36fniHmH+3oG2jG1JsgA6IYisvMikxN2rH99j3aEmZXDHv79+cOBukSuOXSZ96C+
+Udi51SnG0D4bY0/skof6sRNfd64C8wR2spBsdeU8+Hfqp7j4T7P+4o4cSlhkwKFj
+8u96mGDvVPqLaHjVSoRkg3KC7PiOEPIUfp5Uy+kderQ9x9dDXDnnjNBKFX8hoZ/h
+9WXopMUca3c5YQm2yEHbUNGfrsxBGQQVFJd9ByCBaofW8y/AY66aqdHoGn1f9a7e
+H4npkDdq3CPiudP9KmbDVQB7AJOqfqgIAdVNnRe1YUbtd/U+mLdz/UUWTvHy
+-----END RSA PRIVATE KEY-----"
+
 export LOCAL_IP=$(hostname -I)
 export IDENTIFIER=${IDENTIFIER:-"build_$(date +%Y_%m_%d_%H%M%S)"}
 export REPO=${REPO:-"git@github.com:acquia/network-nodejs-uptime-scanner.git"}
@@ -12,7 +64,8 @@ export GITKEY=${GITKEY:-"somekey"}
 
 cd /data
 echo "$GITKEY" > /root/gitkey
-GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa_example -F /dev/null" git clone $REPO /data
+chmod 600 /root/gitkey
+git clone $REPO /data
 
 echo
 echo "--------------------------STARTING SERVICES-----------------------------------"
