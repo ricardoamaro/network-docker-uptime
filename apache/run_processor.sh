cc86cf906d4a3a3c87814bfcbdab1a2440c75bbe
Ricardo Amaro <mail@ricardoamaro.com>
2015-08-21 01:09:49 +0100

    added runners and name fixing

---
+++ apache/run_processor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if we are a member of the "docker" group or have root powers
+USERNAME=$(whoami)
+TEST=$(groups $USERNAME | grep -c '\bdocker\b')
+if [ $TEST -eq 0 ];
+then
+  if [ `whoami` != root ]; then
+    echo "Please run this script as root or using sudo"
+    exit 1
+  fi
+fi
+
+TAG="ricardoamaro/apache"
+APP="dispatcher"
+
+docker run --rm -i  \
+	-e "APP=$APP" \
+	-e "REDIS_HOST=" \
+	-e "REDIS_PORT=" \
+	-e "AUTH_USER=" \
+	-e "AUTH_PASS=" \
+	${TAG}
+
